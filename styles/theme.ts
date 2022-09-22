4805cabd73b0db6a5905e221c4563b7c01ad93ea
Lara Capila <lara.capila@way2.com.br>
2022-09-22 14:44:07 -0300

    build: Configura ESLint, Prettier, EditorConfig, Styled-Components

---
+++ styles/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+	colors: {
+		primary: '#0083ca',
+		secundary: '#fcaf17',
+		success: '#00AF74',
+		error: '#FF4838',
+		mediumGray: '#f4f4f4',
+		darkGray: '#464646',
+		white: '#FFFFFF',
+	},
+	font: {
+		family: {
+			default: "'OpenSans', sans-serif",
+		},
+	},
+};
+
+export default theme;
